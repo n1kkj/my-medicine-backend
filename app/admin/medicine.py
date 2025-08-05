f0e422a48540d6384b20978600372993c2cc5ec8
n1kkj <nikita.lakin.topka@gmail.com>
2025-08-05 21:34:59 +0300

    medicine rework

--- app/admin/medicine.py
+++ app/admin/medicine.py
@@ -5,4 +5,63 @@ from app.models import Medicine
 
 @admin.register(Medicine)
 class MedicineAdmin(admin.ModelAdmin):
-    list_display = ('name', 'release_form', 'quantity_in_package', 'image_path', 'package_count', 'unit', 'user')
+    list_display = (
+        'name',
+        'manufacturer',
+        'release_form',
+        'active_ingredients',
+        'origin_country',
+        'image_path'
+    )
+    list_filter = (
+        'manufacturer',
+        'origin_country',
+        'release_form',
+    )
+    search_fields = (
+        'name',
+        'manufacturer',
+        'active_ingredients',
+        'composition',
+    )
+    fieldsets = (
+        ('Основная информация', {
+            'fields': (
+                'name',
+                'manufacturer',
+                'image_path',
+                'release_form',
+                'quantity_in_package',
+                'origin_country',
+                'barcodes'
+            )
+        }),
+        ('Состав и свойства', {
+            'fields': (
+                'active_ingredients',
+                'composition',
+                'pharmacological_effect',
+                'pharmacokinetics',
+            ),
+            'classes': ('collapse',)
+        }),
+        ('Применение', {
+            'fields': (
+                'indications',
+                'contraindications',
+                'method_application_dosage',
+                'pregnancy_usage',
+            ),
+            'classes': ('collapse',)
+        }),
+        ('Дополнительная информация', {
+            'fields': (
+                'side_effects',
+                'overdose',
+                'drugs_interaction',
+                'storage_conditions',
+            ),
+            'classes': ('collapse',)
+        }),
+    )
+    readonly_fields = ('id',)
--- app/models/medicine.py
+++ app/models/medicine.py
@@ -1,17 +1,32 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from app.models.user import User
-
 
 class Medicine(models.Model):
-    name = models.CharField(_('Название лекарства'), max_length=255)
-    release_form = models.CharField(_('Форма выпуска'), max_length=100, null=True, blank=True)
-    quantity_in_package = models.CharField(_('Количество в упаковке'), max_length=100, null=True, blank=True)
-    image_path = models.CharField(_('Путь к изображению'), max_length=255, null=True, blank=True)
-    package_count = models.IntegerField(_('Количество упаковок'), default=0)
-    unit = models.CharField(_('Единица измерения'), max_length=50, null=True, blank=True)
-    user = models.ForeignKey(User, verbose_name=_('Пользователь'), on_delete=models.CASCADE)
+    id = models.BigAutoField(primary_key=True)
+    image_path = models.CharField(_('Путь к изображению'), max_length=255, blank=True, null=True)
+    name = models.TextField(_('Название лекарства'), blank=True, null=True)
+    manufacturer = models.TextField(_('Производитель'), blank=True, null=True)
+    origin_country = models.CharField(_('Страна происхождения'), max_length=255, blank=True, null=True)
+    release_form = models.CharField(_('Форма выпуска'), max_length=255, blank=True, null=True)
+    active_ingredients = models.TextField(_('Активные ингредиенты'), blank=True, null=True)
+    vacation_procedure = models.TextField(_('Порядок отпуска'), blank=True, null=True)
+    quantity_in_package = models.TextField(_('Количество в упаковке'), blank=True, null=True)
+    barcodes = models.BigIntegerField(_('Штрих-коды'), blank=True, null=True)
+    composition = models.TextField(_('Состав'), blank=True, null=True)
+    description = models.TextField(_('Описание'), blank=True, null=True)
+    pharmacological_effect = models.TextField(_('Фармакологическое действие'), blank=True, null=True)
+    pharmacokinetics = models.TextField(_('Фармакокинетика'), blank=True, null=True)
+    indications = models.TextField(_('Показания'), blank=True, null=True)
+    contraindications = models.TextField(_('Противопоказания'), blank=True, null=True)
+    safety_precautions = models.TextField(_('Меры предосторожности'), blank=True, null=True)
+    pregnancy_usage = models.TextField(_('Применение при беременности'), blank=True, null=True)
+    method_application_dosage = models.TextField(_('Способ применения и дозы'), blank=True, null=True)
+    side_effects = models.TextField(_('Побочные эффекты'), blank=True, null=True)
+    overdose = models.TextField(_('Передозировка'), blank=True, null=True)
+    drugs_interaction = models.TextField(_('Взаимодействие с другими препаратами'), blank=True, null=True)
+    special_instructions = models.TextField(_('Особые указания'), blank=True, null=True)
+    storage_conditions = models.TextField(_('Условия хранения'), blank=True, null=True)
 
     class Meta:
         db_table = 'medicines_table'
